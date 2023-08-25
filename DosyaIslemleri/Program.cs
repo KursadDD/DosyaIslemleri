312704ab9bf75f61a2bf0e9a5b26d41ea1b5ddde
Kursad <kursad.demirdogen@gmail.com>
2023-08-26 01:08:02 +0300

    Add project files.

---
+++ DosyaIslemleri/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DosyaIslemleri
+{
+    class Program
+    {
+        public static void WriteFile()
+        {
+            string path = @"C:\Users\Hp\source\repos\DosyaIslemleri\example.txt";
+            FileInfo fi = new FileInfo(path);
+            Console.WriteLine(fi.FullName);
+        }
+
+        public static void ReadFile() 
+        {
+            string path = @"C:\Users\Hp\source\repos\DosyaIslemleri\example.txt";
+            Console.WriteLine(File.GetLastWriteTime(path)+"11111");
+        }
+        static void Main(string[] args)
+        {
+            WriteFile();
+            ReadFile();
+            Console.ReadKey();
+        }
+    }
+}
